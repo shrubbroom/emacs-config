0c5cde8e4d62e92974abb289b26a030e3e70e71d
Ke Wu <shrubbroom@mac1729.local>
2021-12-17 20:49:36 +0800

    port to mac

--- early-init.el
+++ early-init.el
@@ -5,7 +5,7 @@
  '((horizontal-scroll-bars . nil)       ;; No horizontal scroll-bars
    (left-fringe . 8)                    ;; Thin left fringe
    (right-fringe . 8)                   ;; Thin right fringe
-   (menu-bar-lines . 0)                 ;; No menu bar
+   ;; (menu-bar-lines . 0)              ;; No menu bar, this would make emacs unable to maximaze in mac port
    (tool-bar-lines . 0)                 ;; No tool bar
    (vertical-scroll-bars . nil)))       ;; No vertical scroll-bars
 (defvar init/idle-gc-timer
@@ -13,9 +13,15 @@
                        t
                        'garbage-collect))
 (setq package-enable-at-startup nil)
-(setq package-archives '(("gnu" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
-                         ("melpa" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")
-                         ("org" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/org/")))
+(setq url-proxy-services
+      '(("http" . "localhost:7890")
+        ("https" . "localhost:7890")
+        ("no_proxy" . "0.0.0.0")))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+;; (setq package-archives '(("gnu" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
+;;                          ("melpa" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")
+;;                          ("org" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/org/")))
 (let ((custom (expand-file-name "custom.el" user-emacs-directory)))
  (unless (file-exists-p custom)
    (warn "creating custom.el since it doesn't exist")

0161852b23a474ef7bb2e7e60b35618d452e3e88
shrubbroom <shrubbroom@sjtu.edu.cn>
2022-02-24 23:51:19 +0800

    add test case for non-darwin system

--- early-init.el
+++ early-init.el
@@ -1,13 +1,23 @@
 ;;; early-init.el --- Emacs early init -*- lexical-binding: t; -*-
 (setq gc-cons-threshold 100000000)
-(setq-default
- default-frame-alist
- '((horizontal-scroll-bars . nil)       ;; No horizontal scroll-bars
-   (left-fringe . 8)                    ;; Thin left fringe
-   (right-fringe . 8)                   ;; Thin right fringe
-   ;; (menu-bar-lines . 0)              ;; No menu bar, this would make emacs unable to maximaze in mac port
-   (tool-bar-lines . 0)                 ;; No tool bar
-   (vertical-scroll-bars . nil)))       ;; No vertical scroll-bars
+(if (eq system-type 'darwin)
+ (setq-default
+  default-frame-alist
+  '((horizontal-scroll-bars . nil) ;; No horizontal scroll-bars
+    (left-fringe . 8)              ;; Thin left fringe
+    (right-fringe . 8)             ;; Thin right fringe
+    ;; (menu-bar-lines . 0)              ;; No menu bar, this would make emacs unable to maximaze in mac port
+    (tool-bar-lines . 0)           ;; No tool bar
+    (vertical-scroll-bars . nil))) ;; No vertical scroll-bars
+ (setq-default
+  default-frame-alist
+  '((horizontal-scroll-bars . nil)
+    (left-fringe . 8)
+    (right-fringe . 8)
+    (menu-bar-lines . 0)
+    (tool-bar-lines . 0)
+    (vertical-scroll-bars . nil))))       
+
 (defvar init/idle-gc-timer
   (run-with-idle-timer 20
                        t
